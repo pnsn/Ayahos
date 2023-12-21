63a53c18eb69c8ab38d0068783086530234e19c7
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2023-12-21 14:57:56 -0800

    Updating RingWyrm for I/O flexibility

--- wyrm/core/io.py
+++ wyrm/core/io.py
@@ -49,11 +49,14 @@ Wyrm (ð)
     under an AGPL-3.0 license.
 
 """
+import PyEW
+import pandas as pd
 from collections import deque
 from wyrm.core.wyrm import Wyrm
-from wyrm.core.message import WaveMsg
+from wyrm.core.message import _BaseMsg, TraceMsg
 from obspy import Stream, read
 import fnmatch
+
 
 ##########################
 ### RINGWYRM BASECLASS ###
@@ -80,9 +83,56 @@ class RingWyrm(Wyrm):
     :: ATTRIBUTES :: 
     :attrib module: [PyEW.EWModule]
     :attrib conn_index: [int]
+    :attrib queue_dict: [dict of {sncl: {q:deque, a:age}}] buffer that either stores received
+                        messages from Earthworm or outgoing messages to Earthworm
+                        in a pulse. Each sncl-keyed entry consists of a dictionary 
+                        composed of:
+                            q: deque - [collections.deque] 
+                                    double-ended queue that stores type _BaseMsg + children
+                                    objects
+                            a: age - [int] number of pulses since the last time a new element was
+                                    added to the associated deque
+    :attrib mtype: [str] Earthworm message TYPE_* name
+    :attrib mcode: [int] Earthworm message type integer code
+    :attrib _to_ring: [bool] is data flow from python to earthworm?
+    :attrib _max_pulse_size: [int] maximum number of messages to pull/put per pulse(x)
+                Default: 12000 = 800 stations x 3 channels x 5 pulses x 1 sec-long messages x 1 sec pulse_rate
+    :attrib _max_queue_size: [int] maximum number of messages permitted per queue.
+                Default: 150 = 60 sec windows x 2 non-overlapping windows x 1.25 FOS
+    :attrib _max_queue_age: [int] maximum number of pulses a given queue can go without
+                                receiving new data.
+                Default: 60 = 60 sec window x 1 sec pulse_rate
+        
+    NOTES:
+    queueing rule: FIFO - first in first out 
+                New entries are added to deques with queue.appendleft(x) and entries are 
+                removed from deques with x = queue.pop()
+                    pop() is used by:
+                        + subsequent Wyrms to claim SNCL matched messages
+                        + this Wyrm when clearing/cleaning buffers
+    clean/flush rules:
+        For a given 'SCNL' at the end of a pulse:
+            CLEAN
+            if len(self.queue_dict['SNCL']['q']) > _max_queue_size
+                -> pop entries until len(*) = _max_queue_size
+            FLUSH
+            if self.queue_dict['SNCL']['a'] > _max_queue_age
+                -> clear all entries from self.queue_dict['SCNL']['q']
+                    i.e., self.queue_dict['SCNL']['q'] = deque([])
+
+
     """
 
-    def __init__(self, module, conn_info, msg_type='TYPE_TRACEBUFF2', ew_global_msg_code=0, flow_direction='to python'):
+    def __init__(
+            self,
+            module,
+            conn_info,
+            mtype='TYPE_TRACEBUFF2',
+            mcode=19,
+            flow_direction='to python',
+            max_pulse_size=12000,
+            max_queue_length=150,
+            max_age=60):
         # Run compatability checks on module
         if isinstance(module, PyEW.EWModule):
             self.module = module
@@ -111,9 +161,6 @@ class RingWyrm(Wyrm):
         # Final bit of formatting for __repr__ purposes
         if self.conn_info.index.name is not 'index':
             self.conn_info.index.name = 'index'
-
-        # Initialize dictionary for holding deques
-        self.buffer_dict = {}
         
         # Compatability check for flow_direction
         if flow_direction.lower() in ['from ring','to python','to py','from earthworm','from ew','from c','c2p','ring2py', 'ring2python']:
@@ -121,26 +168,30 @@ class RingWyrm(Wyrm):
         elif flow_direction.lower() in ['from python','to ring','from py','to earthworm','to c','to ew', 'p2c','py2ring','python2ring']:
             self._from_ring = False
         else:
-            raise ValueError('flow direction type {flow_direction} is not supported. See RingWyrm doc')
+            raise ValueError('flow direction type {flow_direction} is not supported. See RingWyrm header doc')
         
-        # Compatability check for message type
-        if isinstance(msg_type, str):
-            if msg_type.lower() == 'tracebuff2':
-                if self._from_ring:
-                    self._msg_method = self.module.
-            
-            elif msg_type.lower() == ['wave']
-
-            elif msg_type.lower() in ['msg','message','string','text']:
-                self._msg_type = 'msg'
-            
-            elf
-            
+        # Comptatability check for message type using wyrm.core.message._BaseMsg
+        try: 
+            test_msg = _BaseMsg(mtype=mtype, mcode=mcode)
+            self.mtype = test_msg.mtype
+            self.mcode = test_msg.mcode
+        except TypeError:
+            raise TypeError(f'from mtype:mcode | {mtype}:{mcode}')
+        except SyntaxError
+            raise SyntaxError(f'from mtype:mcode | {mtype}:{mcode}')
+        except ValueError
+            raise ValueError(f'from mtype:mcode | {mtype}:{mcode}')
+       
+        # Initialize buffer dictionary & limit indices
+        self.queue_dict = {}
+        self._max_pulse_size = max_pulse_size
+        self._max_queue_size = max_queue_length
+        self._max_queue_age = max_age
 
     def __repr__(self):
         rstr = f"Module: {self.module}\n"
         rstr += f"Conn Info: {self.conn_info}\n"
-        rstr += f"No. Buffers: {len(self.buffer_dict)}"
+        rstr += f"No. Queues: {len(self.queue_dict)}"
         return rstr
 
     def change_conn_info(self, new_conn_info):
@@ -169,6 +220,42 @@ class RingWyrm(Wyrm):
         :: OUTPUT ::
         :return y: variable accessing this RingWyrm's self.queue_dict attribute
         """
+        # If flowing to a ring (PY->EW)
+        if self._to_ring:
+            # If working with wave-like messaging, use class-methods
+            # written into TraceMsg
+            if self.mtype == 'TYPE_TRACEBUF2' and self.mcode == 19:
+                # Iterate across all _sncl
+                deltas = {}
+                for _sncl in self.queue_dict:
+                    _qad = self.queue_dict[_sncl]
+                    _qlen = len(_qad['q'])
+                    if _qlen > 0:
+                        for _ in range(_qlen):
+                            # Pop off _msg
+                            _msg = _qad['q'].pop()
+                            # If instance of TraceMsg
+                            if isinstance(_msg, TraceMsg):
+                                # Send to EW
+                                _msg.to_ew(self._conn_index)
+                            # append value back to end of queue
+                            else:
+                                _qad['q'].appendleft(_msg)
+                    delta = _qlen - len(_qad['q'])
+                    deltas.update({_sncl:delta)})
+                                
+
+                        
+            else:
+                NotImplementedError('Other mtype:mcode combination handling not yet developed')
+        # If flowing from a ring (EW->PY)
+        elif self._to_ring:
+
+
+
+        else:
+            raise RuntimeError('Dataflow direction from self._to_ring not valid')
+
         y = self.queue_dict
         return y
             

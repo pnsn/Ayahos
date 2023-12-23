b2ad83bdf1b4f723de152e0d804aecfef7431caf
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2023-12-22 16:18:50 -0800

    Start adding DEQ_Dict class inheritance to InWyrms

--- wyrm/core/io.py
+++ wyrm/core/io.py
@@ -49,19 +49,21 @@ Wyrm (ð)
     under an AGPL-3.0 license.
 
 """
-import PyEW
+import os
 import pandas as pd
+from glob import glob
 from collections import deque
 from wyrm.core.wyrm import Wyrm
-from wyrm.core.message import _BaseMsg, TraceMsg
+from wyrm.core.message import _BaseMsg, TraceMsg, StreamMsg, DEQ_Dict
 from obspy import Stream, read
 import fnmatch
+import PyEW
 
 
 ##########################
 ### RINGWYRM BASECLASS ###
 ##########################
-class RingWyrm(Wyrm):
+class RingWyrm(Wyrm, DEQ_Dict):
     """
     Base class provides attributes to house a single PyEW.EWModule
     connection to an Earthworm RING.
@@ -133,6 +135,11 @@ class RingWyrm(Wyrm):
             max_pulse_size=12000,
             max_queue_length=150,
             max_age=60):
+        
+        # Initialize parent classes
+        Wyrm.__init__(self)
+        DEQ_Dict.__init__(self)
+
         # Run compatability checks on module
         if isinstance(module, PyEW.EWModule):
             self.module = module
@@ -177,13 +184,12 @@ class RingWyrm(Wyrm):
             self.mcode = test_msg.mcode
         except TypeError:
             raise TypeError(f'from mtype:mcode | {mtype}:{mcode}')
-        except SyntaxError
+        except SyntaxError:
             raise SyntaxError(f'from mtype:mcode | {mtype}:{mcode}')
-        except ValueError
+        except ValueError:
             raise ValueError(f'from mtype:mcode | {mtype}:{mcode}')
        
         # Initialize buffer dictionary & limit indices
-        self.queue_dict = {}
         self._max_pulse_size = max_pulse_size
         self._max_queue_size = max_queue_length
         self._max_queue_age = max_age
@@ -191,7 +197,7 @@ class RingWyrm(Wyrm):
     def __repr__(self):
         rstr = f"Module: {self.module}\n"
         rstr += f"Conn Info: {self.conn_info}\n"
-        rstr += f"No. Queues: {len(self.queue_dict)}"
+        rstr += f"{DEQ_Dict.__repr__(self)}"
         return rstr
 
     def change_conn_info(self, new_conn_info):
@@ -340,35 +346,35 @@ class RingWyrm(Wyrm):
             
 
 
-class Disk2PyWyrm(Wyrm):
+class Disk2PyWyrm(Wyrm, DEQ_Dict):
     """
     This Wyrm provides a pulsed behavior that reads waveform files from disk and 
     presents a queue_dict attribute with identical formatting as RingWyrm, providing
     a 
     
     """
-    def __init__(self, dir, file_queue=deque([]), file_format='MSEED', max_pulse_size=50, max_queue_size=50, max_age=50):
+    def __init__(self, fdir, file_glob_str='*.mseed', file_format='MSEED', max_pulse_size=50, max_queue_size=50, max_age=50):
+        Wyrm.__init__(self)
         # Initialize queue_dict
-        self.queue_dict = {}
+        DEQ_Dict.__init__(self)
 
         # Compatability check on dir
-        if not isinstance(dir, str):
-            raise TypeError('dir must be type str')
-        elif not os.path.exists(dir):
-            raise FileExistsError('Directory "dir" does not exist')
+        if not isinstance(fdir, str):
+            raise TypeError('fdir must be type str')
+        elif not os.path.exists(fdir):
+            raise FileExistsError(f'Directory {fdir} does not exist')
         else:
-            self.dir = dir
+            self.fdir = fdir
 
-        # Compatability checks on file_queue
-        if isinstance(file_queue, (list, deque)):
-            if all(isinstance(file, str) for file in file_queue):
-                self.file_queue = deque(file_queue)
-            else:
-                raise TypeError('All elements of file_queue must be type str')
-        elif isinstance(file_queue, str):
-            self.file_queue = deque([file_queue])
+        if not isinstance(file_glob_str, str):
+            raise TypeError('file_glob_str must be type str')
         else:
-            raise TypeError('file_queue must be type str or a list or deque of strings')
+            flist = glob(os.path.join(self.fdir,file_glob_str))
+            if len(flist) == 0:
+                raise SyntaxError(f"No files glob'd with {os.path.join(self.fdir, file_glob_str)}")
+            else:
+                flist.sort()
+                self.file_queue = deque(flist)
 
         # Compatability Check on file_format
         if isinstance(file_format, str):
@@ -393,9 +399,9 @@ class Disk2PyWyrm(Wyrm):
             raise TypeError
         
     def __repr__(self):
-        rstr = f"Source Dir: {self.dir}\n"
+        rstr = f"Source Dir: {self.fdir}\n"
         rstr += f"No. Queued Files: {len(self.file_queue)}\n"
-        rstr += f"Unique SNCL in Data Queue: {len(self.queue_dict)}"
+        rstr += DEQ_Dict.__repr__(self)
         return rstr
 
     
@@ -403,6 +409,7 @@ class Disk2PyWyrm(Wyrm):
         """
         Load up to max_pulse_size files from disk
         """
+        x = x
         for _ in range(self.max_pulse_size):
             # If file queue is empty
             if len(self.file_queue) == 0:
@@ -410,14 +417,12 @@ class Disk2PyWyrm(Wyrm):
             else:
                 # Pop file name from file_queue
                 _file = self.file_queue.pop()
-                # Compose file path
-                _path_file = os.path.join(self.dir, _file)
                 # Try to load file
                 try:
-                    _st = read(_path_file)
+                    _st = read(_file)
                 # Propagate TypeError from obspy.read if it raises TypeError
                 except TypeError:
-                    raise TypeError(f'something went wrong loading {_path_file}')
+                    raise TypeError(f'something went wrong loading {_file}')
                 # NOTE: For development only - must clean up hanging except
                 except:
                     print('SOMETHING ELSE WENT WRONG WITH obspy.read')
@@ -430,17 +435,151 @@ class Disk2PyWyrm(Wyrm):
                         _trMsg = TraceMsg(_tr, dtype=_tr.data.dtype)
                     # Failing that, interpret as float32
                     except TypeError:
-                        _trMsg = TraceMsg(_tr, dtype = 'f4')
+                        _trMsg = TraceMsg(_tr, dtype='f4')
+                    # Use DEQ_Dict append to add 
+                    self.append_msg_left(_trMsg)
 
-                    if _trMsg.sncl not in self.queue_dict.keys():
-                        
-        # Present access to self.queue_dict as output
-        y = self.queue_dict
+        # Present access to self.queues as a dictionary
+        y = self.queues
         return y
 
 class Py2DiskWyrm(Wyrm):
+    def __init__(self,
+                 root_dir,
+                 sncl_filter='*.*.*.*',
+                 save_format='{NET}/{STA}/{SNCL}_{TS}_{TE}.mseed',
+                 max_pulse_size=1000):
+
+        # Compatability checks for root_dir
+        if not isinstance(root_dir,str):
+            raise TypeError('root_dir must be type str')
+        elif not os.path.exists(root_dir):
+            os.makedirs(root_dir)
+            print(f'Generated {root_dir}')
+            self.root_dir = root_dir
+        else:
+            self.root_dir = root_dir
+
+        # Compatability checks for sncl_filter
+        if not isinstance(sncl_filter, str):
+            raise TypeError('sncl filter must be type str')
+        elif len(sncl_filter.split('.')) != 4:
+            raise SyntaxError('sncl filter should be four "." delimited strings, minimally *.*.*.*')
+        else:
+            self.sncl_filter = sncl_filter
+
+        # Compatability checks for save_format
+        if not isinstance(save_format, str):
+            raise TypeError('save_format must be type str')
+        else:
+            self.save_format=save_format
+        
+        # Compatability checks for max_pulse_size
+        if not isinstance(max_pulse_size, (int, float)):
+            raise TypeError('max_pulse_size must be int-like')
+        else:
+            self.max_pulse_size = int(max_pulse_size)
+
+    def __repr__(self):
+        rstr = f'OUTPUT FMT: {os.path.join(self.root_dir, self.save_format)}\n'
+        rstr += f'SNCL Filt: {self.sncl_filter} | '
+        rstr += f'MAX Pulse: {self.max_pulse_size}\n'
+        return rstr
+
+
+    def pulse(self, x):
+        """
+        Pulse this Py2DiskWyrm with input of a DEQ_Dict.queues dictionary
+        and prune off TraceMsg entries for S.N.C.L keyed queues that match
+        self.sncl_filter
+
+        :: INPUT ::
+        :param x: [dict] from a DEQ_Dict (it's queues attribute)
+        
+        :: OUTPUT ::
+        :return y: [None]
+        """
+
+        # Fitler for matching keys
+        sncl_list = fnmatch.filter(x.keys(), self.sncl_filter):
+        # Get number of sncl entries
+        _j = len(sncl_list)
+        # Get count of total elements
+        _e = sum([len(x[_sncl]['q']) for _sncl in sncl_list])
+        # Iterate for max_pulse_size
+        for _i in range(self.max_pulse_size):
+            # If total elements hits 0, break iteration
+            if _e <= 0:
+                break
+            # Otherwise, proceed
+            else:
+                # Use modulus index to wrap sncl list if it's shorter than max_iter
+                _sncl = sncl_list[_i%_j]
+                # If this queue is empty, continue iterating
+                if len(x[_sncl]['q']) == 0:
+                    continue
+                # If queue is not empty, pop message
+                else:
+                    _msg = x[_sncl]['q'].pop()
+                    # If message is a wyrm *Msg
+                    if isinstance(_msg, _BaseMsg):
+                        # If the message is not a TraceMsg
+                        if not isinstance(_msg, TraceMsg):
+                            # Append the item back on the list
+                            x[_sncl]['q'].appendleft(_msg)
+                            # Do not decrease element counter
+                            _e -= 0
+                        # If the message is a TraceMsg, proceed with saving
+                        else:
+                            # Format file name
+                            fname = self.save_format.format(NSLC=_msg.id,
+                                                            SNCL=_msg.sncl,
+                                                            STA=_msg.stats.station,
+                                                            NET=_msg.stats.network,
+                                                            CHA=_msg.stats.channel,
+                                                            LOC=_msg.stats.location,
+                                                            TS=_msg.stats.starttime,
+                                                            TSY=_msg.stats.starttime.year,
+                                                            TSJ=_msg.stats.starttime.julday,
+                                                            TSH=_msg.stats.starttime.hour,
+                                                            TSm=_msg.stats.starttime.minute,
+                                                            TSs=_msg.stats.starttime.second,
+                                                            TE=_msg.stats.endtime,
+                                                            TEY=_msg.stats.endtime.year,
+                                                            TEJ=_msg.stats.endtime.julday,
+                                                            TEH=_msg.stats.endtime.hour,
+                                                            TEm=_msg.stats.endtime.minute,
+                                                            TEs=_msg.stats.endtime.second)
+                            # Compose full file path/name
+                            fnp = os.path.join(self.root_dir, fname)
+                            # Extract path
+                            path = os.path.split(fnp)
+                            # Create path if it doesn't exist
+                            if not os.path.exists(path):
+                                os.makedirs(path)
+                            # Write to disk
+                            _msg.write(fnp)
+                            # Decrease element count by 1
+                            _e -= 1
+        y = None
+        return y
+
 
         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #########################
 ### RINGWYRM CHILDREN ###
 #########################

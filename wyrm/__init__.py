a35c0cda7206e0bdaf44ef67f11ef5350687d69a
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2023-12-27 14:11:59 -0800

    Add __init__

---
+++ wyrm/__init__.py
@@ -0,0 +1,9 @@
+# Give import aliases for Wyrms
+from wyrm.wyrms.wyrm import Wyrm
+from wyrm.message.base import _BaseMsg
+
+from wyrm.structures.queues import DEQ_Dict
+# from wyrm.message.trace import TraceMsg
+# # from wyrm.wyrms.ringwyrm import RingWyrm
+
+# # from wyrm.wyrms.heartwyrm import Heartwyrm

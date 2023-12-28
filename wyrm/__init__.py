eebc4604be1061d8cf6d5db65ce989b478d48ecc
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2023-12-28 13:58:12 -0800

    Refactor depends

--- wyrm/__init__.py
+++ wyrm/__init__.py
@@ -2,7 +2,7 @@
 from wyrm.wyrms.wyrm import Wyrm
 from wyrm.message.base import _BaseMsg
 
-from wyrm.structures.queues import DEQ_Dict
+from wyrm.structures.deq_dict import DEQ_Dict
 # from wyrm.message.trace import TraceMsg
 # # from wyrm.wyrms.ringwyrm import RingWyrm
 

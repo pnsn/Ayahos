56a4404919cb73b4f512bef65fe317de6613193f
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2024-01-03 11:46:23 -0800

    Add rtbufftrace

--- wyrm/__init__.py
+++ wyrm/__init__.py
@@ -1,8 +1,8 @@
 # Give import aliases for Wyrms
 from wyrm.wyrms.wyrm import Wyrm
 from wyrm.message.base import _BaseMsg
-
-from wyrm.structures.deq_dict import DEQ_Dict
+from wyrm.structures.rtbufftrace import RtBuffTrace
+# from wyrm.structures.deq_dict import DEQ_Dict
 # from wyrm.message.trace import TraceMsg
 # # from wyrm.wyrms.ringwyrm import RingWyrm
 

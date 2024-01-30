6ca8f35d626d80c7c16e1f994ac48ef15c18c0c1
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2024-01-29 16:36:27 -0800

    Revise pulse subroutines and output strcutrue

--- wyrm/wyrms/seisbenchwyrm.py
+++ wyrm/wyrms/seisbenchwyrm.py
@@ -7,6 +7,8 @@ from wyrm.structures.window import InstWindow
 import seisbench.models as sbm
 import numpy as np
 import torch
+from tqdm import tqdm
+from time import time
 
 class WaveformModelWyrm(Wyrm):
     """
@@ -65,7 +67,7 @@ class WaveformModelWyrm(Wyrm):
             try:
                 device = torch.device(devicetype)
             except RuntimeError:
-                raise RintimeError(f'devicetype {devicetype} is an invalid device string for PyTorch')
+                raise RuntimeError(f'devicetype {devicetype} is an invalid device string for PyTorch')
             try:
                 self.model.to(device)
             except RuntimeError:
@@ -82,7 +84,12 @@ class WaveformModelWyrm(Wyrm):
 
         # Initialize Realtime Instrument Stream in PredTrace mode
         # self.buffer = RtInstStream(trace_type=RtPredTrace, max_length=max_length)
-        self.queue = deque([])
+        # self.queue = deque([])
+
+        # Start layered dict tree {NSLI}{meta}{metadata}
+        #                               {wgt1}{tensor}
+        #                               {wgt2}{tensor}
+        self.tree = {}
 
     def pulse(self, x):
         """
@@ -93,12 +100,52 @@ class WaveformModelWyrm(Wyrm):
         :return y: [RtInstStream] access to RtInstStream composed
                 of RtPredTrace objects.
         """
+        # Construct prediction inputs
+        window_concat_tensor, meta_list = self._instwindows2tensor(x)
+        # Iterate across weight_names
+        for _n in self.weight_names:
+            if self.debug:
+                print('Starting {self.model_name}, {_n} weighted prediction')
+            # Ensure correct model weights are loaded
+            self.model.from_pretrained(_n)
+            print(_n)
+            # if self.model.weights_docstring is None:
+            #     self.model.from_pretrained(_n)
+            # elif _n not in self.model.weights_docstring:
+            #     self.model.from_pretrained(_n)
+            raw_preds = self._run_prediction(window_concat_tensor)
+            npy_preds = self._raw_preds2numpy(window_concat_tensor, raw_preds)
+            if self.debug:
+                print(f'Appending to tree ({time() - tick})')
+            # Iterate across windows and append to tree
+            for _i, _meta in enumerate(meta_list):
+                self._append_to_tree(_meta, npy_preds[_i, :, :], _n)
+        self._sort_tree_indices()
+        y = self.tree
+        return y
+
+    def _instwindows2tensor(self, x):
+        """
+        Convert a deque of InstWindow objects into
+        a concatenated torch.Tensor of up to self.max_pulse_size
+        windows of pre-processed instrument data
+
+        :: INPUT ::
+        :param x: [deque] of [wyrm.structures.window.InstWindow] objects
+                  that have been pre-processed
+        :: OUTPUTS ::
+        :return window_concat_tensor: [torch.Tensor] with SeisBench dimensions
+                                    (window#, component, data)
+        :return meta_list: [list] of [dict] containing window information
+                            that correspond with the 0-axis indexing of
+                            window_concat_tensor
+        """
         if not isinstance(x, deque):
             raise TypeError('input x must be a deque')
         # Concatenate input tensors and metadata
         tensor_list = []
         meta_list = []
-        for _i in range(self.max_pulse_size):
+        for _ in range(self.max_pulse_size):
             if len(x) > 0:
                 _x = x.pop()
                 if not isinstance(_x, InstWindow):
@@ -108,51 +155,74 @@ class WaveformModelWyrm(Wyrm):
                 tensor_list.append(tensor)
                 meta_list.append(meta)
         
-        model_inputs = torch.concat(tensor_list)
+        window_concat_tensor = torch.concat(tensor_list)
+        return window_concat_tensor, meta_list
+    
+    def _run_prediction(self, tensor):
+        """
+        Run prediction on an torch.Tensor composed of
+        concatenated, windowed tensors
 
-        # Iterate across specified model_weight(s)
-        for _n in self.weight_names:
-            # Load model weights
-            if self.model.weights_docstring is None:
-                self.model.from_pretrained(_n)
-            elif _n not in self.model.weights_docstring:
-                self.model.from_pretrained(_n)
-            # Ensure model is on specified device
-            if self.model.device.type != self.device.type:
-                self.model.to(self.device)
-            ## !!! PREDICT !!! with check that data are on device ##
-            if model_inputs.device.type == self.device.type:
-                raw_preds = self.model(model_inputs)
+        :
+        """
+        if self.model.device.type != self.device.type:
+            self.model.to(self.device)
+        if tensor.device.type == self.device.type:
+            raw_preds = self.model(tensor)
+        else:
+            raw_preds = self.model(tensor.to(self.device))
+        return raw_preds
+    
+    def _raw_preds2numpy(self, window_concat_tensor, raw_preds):
+        npy_preds = np.full(window_concat_tensor.shape,
+                            fill_value=np.nan,
+                            dtype=np.float32)
+        for _j, _p in enumerate(raw_preds):
+            if _p.device.type != 'cpu':
+                npy_preds[:, _j, :] = _p.detach().cpu().numpy()
             else:
-                raw_preds = self.model(model_inputs.to(self.device))
-            
-            ## Detach predictions and split into tuples to pass onward
-            npy_preds = np.full(
-                shape=model_inputs.shape,
-                fill_value=np.nan,
-                dtype=np.float32
-            )
-            for _j, _p in enumerate(raw_preds):
-                if _p.device.type != 'cpu':
-                    npy_preds[:, _j, :] = _p.detach().cpu().numpy()
+                npy_preds[:, _j, :] = _p.detach().numpy()
+        return npy_preds
+    
+
+    def _append_to_tree(self, meta, pred, wgt_name):
+        inst_code = meta['inst_code']
+        idx = meta['index']
+        # If completely new instrument branch
+        if inst_code not in self.tree.keys():
+            self.tree.update({inst_code: {'metadata': {idx: meta}, wgt_name: {idx: pred}}})
+        # for preexisting instrument branch
+        else:
+            # for new weight_name limb
+            if wgt_name not in self.tree[inst_code].keys():
+                self.tree[inst_code].update({wgt_name: {idx: pred}})
+            # for existing weight_name limb
+            elif idx not in self.tree[inst_code][wgt_name].keys():
+                self.tree[inst_code][wgt_name].update({idx: pred})
+            # Raise error if trying to overwrite pre-existing entry
+            else:
+                if self.debug:
+                    print(f'KeyError(Attempting to insert data in pre-existing prediction index {inst_code} {wgt_name} {idx})')
+                    breakpoint()
                 else:
-                    npy_preds[:, _j, :] = _p.detach().numpy()
-            # Split out windows and re-associate metadata with some updates
-            for _i, _pred in enumerate(npy_preds):
-                ometa = meta_list[_i].copy()
-                ometa.update({'weight_name': _n})
-                ometa.update({'label_codes': self.label_codes})
-                # Compose output dictionary
-                okey = f"{ometa['inst_code']}|{ometa['model_name']}|{ometa['weight_name']}"
-                out_branch = {
-                    okey: {
-                        'data': _pred,
-                        'meta': ometa,
-                        'index': ometa['index']
-                    }
-                }
-                # Append to queue
-                self.queue.appendleft(out_branch)
+                    self.tree[inst_code][wgt_name].update({idx: pred})
+            # Do check if new metadata
+            if idx not in self.tree[inst_code]['metadata'].keys():
+                self.tree[inst_code]['metadata'].update({idx: meta})
+        # # Cleanup
+        # self._sort_tree_indices()
+        return self
+
+    def _sort_tree_indices(self):
+        # Iterate across inst_code's
+        for k1 in self.tree.keys():
+            branch = self.tree[k1]
+            # Iterate across metadata and model_weight's
+            for k2 in branch.keys():
+                # Sort by index
+                self.tree[k1][k2] = dict(sorted(branch[k2].items()))
+        return self
+
             
     def __repr__(self):
         # rstr = super().__repr__()
@@ -161,12 +231,64 @@ class WaveformModelWyrm(Wyrm):
         rstr += f'(1, {len(self.model.labels)}, {self.model.in_samples}) | '
         rstr += f'labels: {self.model.labels} (codes: {self.label_codes})\n'
         rstr += f'batch size: {self.max_pulse_size} | '
-        rstr += f'queue length: {len(self.queue)}\n'
-        rstr += f' model weight names:\n'
-        for _n in self.weight_names:
-            rstr += f'    {_n}\n'
+        # rstr += f'queue length: {len(self.queue)}\n'
+        rstr += f' model weight names: '
+        for _n in enumerate(self.weight_names):
+            rstr += f'{_n}'
         return rstr
 
     def __str__(self):
         rstr = self.__repr__()
         return rstr
+    
+
+
+    #  npy_preds = np.full(
+    #         shape = tensor.shape,
+    #         fill_value=np.nan,
+    #         dtype=np.float32
+    #     )
+
+    #     # Iterate across specified model_weight(s)
+    #     for _n in self.weight_names:
+    #         # Load model weights
+    #         if self.model.weights_docstring is None:
+    #             self.model.from_pretrained(_n)
+    #         elif _n not in self.model.weights_docstring:
+    #             self.model.from_pretrained(_n)
+    #         # Ensure model is on specified device
+    #         if self.model.device.type != self.device.type:
+    #             self.model.to(self.device)
+    #         ## !!! PREDICT !!! with check that data are on device ##
+    #         if model_inputs.device.type == self.device.type:
+    #             raw_preds = self.model(model_inputs)
+    #         else:
+    #             raw_preds = self.model(model_inputs.to(self.device))
+            
+    #         ## Detach predictions and split into tuples to pass onward
+    #         npy_preds = np.full(
+    #             shape=model_inputs.shape,
+    #             fill_value=np.nan,
+    #             dtype=np.float32
+    #         )
+    #         for _j, _p in enumerate(raw_preds):
+    #             if _p.device.type != 'cpu':
+    #                 npy_preds[:, _j, :] = _p.detach().cpu().numpy()
+    #             else:
+    #                 npy_preds[:, _j, :] = _p.detach().numpy()
+    #         # Split out windows and re-associate metadata with some updates
+    #         for _i, _pred in enumerate(npy_preds):
+    #             ometa = meta_list[_i].copy()
+    #             ometa.update({'weight_name': _n})
+    #             ometa.update({'label_codes': self.label_codes})
+    #             # Compose output dictionary
+    #             okey = f"{ometa['inst_code']}|{ometa['model_name']}|{ometa['weight_name']}"
+    #             out_branch = {
+    #                 okey: {
+    #                     'data': _pred,
+    #                     'meta': ometa,
+    #                     'index': ometa['index']
+    #                 }
+    #             }
+    #             # Append to queue
+    #             self.queue.appendleft(out_branch)

1c13d39fdae9998c54f92e9604773d0ed7a5fbf0
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2024-02-06 16:42:20 -0800

    Run PhaseNet check

--- local_dev_quickstart.py
+++ local_dev_quickstart.py
@@ -8,17 +8,22 @@ from tqdm import tqdm
 import wyrm.structures.rtpredbuff as rtp
 import numpy as np
 import matplotlib.pyplot as plt
+from copy import deepcopy
 
 # Initialize model
-model = sbm.EQTransformer()
+print('loading SeisBench Model')
+model = sbm.PhaseNet()
+wgt = 'diting'
 # load data from M4.3 near Port Townsend, 2023
 print('loading data')
 st = read('example/uw61965081/bulk.mseed')
 # Convert stream into inststream
+print('converting to InstStream')
 rtis = RtInstStream(max_length=300).append(st)
 # Initialize wyrms
+print('initializing wyrms')
 windwyrm = WindowWyrm(fill_value=0.).set_windowing_from_seisbench(model)
-sbwyrm = WaveformModelWyrm(model, ['pnw'], devicetype='mps', max_pulse_size=10000)
+sbwyrm = WaveformModelWyrm(model, [wgt], devicetype='mps', max_pulse_size=10000)
 # Pulse windwyrm
 print('windowing data')
 y = windwyrm.pulse(rtis)
@@ -26,6 +31,7 @@ y = windwyrm.pulse(rtis)
 print('preprocessing data')
 for _y in tqdm(y):
     _y._preproc_example()
+y_backup = deepcopy(y)
 # Pulse seisbenchwyrm
 print('running predictions')
 z = sbwyrm.pulse(y)
@@ -33,4 +39,4 @@ z = sbwyrm.pulse(y)
 # Get example data for stacking
 zz = z['CC.ARAT..BH']
 mm = zz['metadata']
-pp = zz['pnw']
+pp = zz[wgt]

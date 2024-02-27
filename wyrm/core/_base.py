61e39525403b309e8a95aa0c7f958f7a5b24a59d
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2024-02-26 16:29:04 -0800

    Remove itype otype methods

--- wyrm/core/_base.py
+++ wyrm/core/_base.py
@@ -25,16 +25,11 @@ class Wyrm(object):
     + __str__
     + __repr__
     + pulse
-    - _matches_itype
-    - _matches_otype
-    - _update_io_types
 
     And attributes:
     @ max_pulse_size - reference integer for maximum number of iterations for
                         the outer loop of a given pulse() method
     @ debug - bool switch for running the wyrm in debug mode (in development)
-    @ _in_type - expected type(s) for inputs to self.pulse()
-    @ _out_type - expected type(s) for outputs from self.pulse()
     """
 
     def __init__(self, max_pulse_size=None, debug=False):
@@ -52,9 +47,9 @@ class Wyrm(object):
             self.max_pulse_size = None
         else:
             self.max_pulse_size = wcc.bounded_intlike(max_pulse_size, name='max_pulse_size', minimum=1)
-        # input and output type for pulse
-        self._in_type = (int, str, float, bool, type(None), type)
-        self._out_type = (int, str, float, bool, type(None), type)
+        # # input and output type for pulse
+        # self._in_type = (int, str, float, bool, type(None), type)
+        # self._out_type = (int, str, float, bool, type(None), type)
 
     def __repr__(self):
         """
@@ -71,56 +66,56 @@ class Wyrm(object):
         rstr = f'wyrm.wyrms.base.Wyrm(max_pulse_size={self.max_pulse_size}, debug={self.debug})'
         return rstr
     
-    def _update_io_types(self, itype=None, otype=None):
-        """
-        --- PRIVATE METHOD ---
-        Update the reference type(s) for the input and output of self.pulse()
+    # def _update_io_types(self, itype=None, otype=None):
+    #     """
+    #     --- PRIVATE METHOD ---
+    #     Update the reference type(s) for the input and output of self.pulse()
 
-        :: INPUTS ::
-        :param itype: [type], [NoneType], or [tuple] thereof
-                        None input results in no change to _in_type
-        :param otype: [type], [NoneType], or [tuple] thereof
-                        None input results in no change to _out_type
-        :: OUTPUT ::
-        :return self: [Wyrm] return self to enable cascading
-        """
-        if itype is not None and isinstance(itype, type):
-            self._in_type = itype
-        elif isinstance(itype, tuple):
-            if all(isinstance(_it, (type, type(None))) for _it in itype):
-                self._in_type = itype
-        if otype is not None and isinstance(otype, type):
-            self._out_type = otype
-        elif isinstance(otype, tuple):
-            if all(isinstance(_it, (type, type(None))) for _it in otype):
-                self._out_type = otype
-        return self
+    #     :: INPUTS ::
+    #     :param itype: [type], [NoneType], or [tuple] thereof
+    #                     None input results in no change to _in_type
+    #     :param otype: [type], [NoneType], or [tuple] thereof
+    #                     None input results in no change to _out_type
+    #     :: OUTPUT ::
+    #     :return self: [Wyrm] return self to enable cascading
+    #     """
+    #     if itype is not None and isinstance(itype, type):
+    #         self._in_type = itype
+    #     elif isinstance(itype, tuple):
+    #         if all(isinstance(_it, (type, type(None))) for _it in itype):
+    #             self._in_type = itype
+    #     if otype is not None and isinstance(otype, type):
+    #         self._out_type = otype
+    #     elif isinstance(otype, tuple):
+    #         if all(isinstance(_it, (type, type(None))) for _it in otype):
+    #             self._out_type = otype
+    #     return self
 
-    def _matches_itype(self, arg, raise_error=False):
-        if not isinstance(arg, self._in_type):
-            if raise_error:
-                emsg = f'arg type {type(arg)} does not match expected type(s): {self._in_type}'
-                raise TypeError(emsg)
-            else:
-                return False
-        else:
-            if raise_error:
-                pass
-            else:
-                return True
+    # def _matches_itype(self, arg, raise_error=False):
+    #     if not isinstance(arg, self._in_type):
+    #         if raise_error:
+    #             emsg = f'arg type {type(arg)} does not match expected type(s): {self._in_type}'
+    #             raise TypeError(emsg)
+    #         else:
+    #             return False
+    #     else:
+    #         if raise_error:
+    #             pass
+    #         else:
+    #             return True
     
-    def _matches_otype(self, arg, raise_error=False):
-        if not isinstance(arg, self._out_type):
-            if raise_error:
-                emsg = f'arg type {type(arg)} does not match expected type(s): {self._out_type}'
-                raise TypeError(emsg)
-            else:
-                return False
-        else:
-            if raise_error:
-                pass
-            else:
-                return True
+    # def _matches_otype(self, arg, raise_error=False):
+    #     if not isinstance(arg, self._out_type):
+    #         if raise_error:
+    #             emsg = f'arg type {type(arg)} does not match expected type(s): {self._out_type}'
+    #             raise TypeError(emsg)
+    #         else:
+    #             return False
+    #     else:
+    #         if raise_error:
+    #             pass
+    #         else:
+    #             return True
 
     def copy(self):
         """

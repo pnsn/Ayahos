85677b6e4b25323d13ce83f9eed9287a110b3d7d
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2024-02-28 16:33:25 -0800

    Still tryin to make this work...

--- wyrm_logging_example.py
+++ wyrm_logging_example.py
@@ -4,28 +4,33 @@ import wyrm
 # logging.config.fileConfig('./wyrm_logging_example.ini')
 # wyrm = wyrm.core._base.Wyrm(debug=True)
 # y = wyrm.pulse(x=2)
+def main():
+    import wyrm
+    logger = logging.getLogger('wyrm_logging_example')
+    logger.setLevel(logging.DEBUG)
 
-logger = logging.getLogger('wyrm_logging_example')
-logger.setLevel(logging.DEBUG)
+    fh = logging.FileHandler('wyrm_logging_example.log')
+    fh.setLevel(logging.DEBUG)
 
-fh = logging.FileHandler('wyrm_logging_example.log')
-fh.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
 
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
 
-fh.setFormatter(formatter)
-ch.setFormatter(formatter)
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+    mywyrm = wyrm.Wyrm.__init__()
 
-logger.addHandler(fh)
-logger.addHandler(ch)
-wyrm = wyrm.Wyrm()
+    logger.info('calling wyrm.pulse')
+    y = mywyrm.pulse(2)
+    print(y)
 
-logger.info('calling wyrm.pulse')
-y = wyrm.pulse(2)
-print(y)
+
+if __name__ == '__main__':
+    main()
 
 # # logger.info('creating an instance of wyrm.core._base.Wyrm')
 # wyrm = wyrm.core._base.Wyrm(max_pulse_size=2, debug=True)

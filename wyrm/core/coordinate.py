032743ba6b369d940659c0ef44eee2cef4b9ef1a
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2024-04-02 16:05:51 -0700

    Start BufferWyrm update for MLTraceBuffer compat

--- wyrm/core/coordinate.py
+++ wyrm/core/coordinate.py
@@ -580,98 +580,122 @@ class BufferWyrm(Wyrm):
     Class for buffering/stacking MLTrace objects into a DictStream of MLTraceBuffer objects with
     self-contained settings and sanity checks for changing the MLTraceBuffer.__add__ method options.
     """
-    def __init__(self, max_length=300., add_method=3, restrict_past_append=True, blinding=(0,0), max_pulse_size=10000, debug=False, **add_kwargs):
+    def __init__(
+            self,
+            buffer_key='id',
+            max_length=300.,
+            restrict_past_append=True,
+            blinding=None,
+            method=1,
+            max_pulse_size=10000,
+            debug=False,
+            **add_kwargs):
         """
-        Initialize a BufferWyrm object
+        Initialize a BufferWyrm object that contains attributes that house a DictStream that comprises 
+        MLTraceBuffer objects keyed by trace IDs. 
+        
 
         :: INPUTS ::
+        ~~~MLTraceBuffer Initilizing Inputs~~~
         :param max_length: [float] maximum MLTraceBuffer length in seconds
-        :param add_style: [str] style of handling overlapping samples in a given MLTraceBuffer
-                            Supported:
-                                'merge' - follows the obspy.core.trace.Trace.__add__(method=1, interpolation_samples=-1)
-                                            style of behaviors, linearly interpolating between end-point samples in
-                                            a given overlap.
-                                        NOTE: Generally used for stitching together waveform data
-                                'stack' - stacks overlapping samples either using the 'max' or 'avg' approach typically
-                                            used for overlapping predicted values from machine learning models
         :param restrict_past_add: [bool] - enforce restrictions on appending data that temporally preceed data contained
-                            within MLTraceBuffer objects?  also see wyrm.data.mltrace.MLTraceBuffer.__add__
+                            within MLTraceBuffer objects? 
+                            ~also see wyrm.data.mltracebuffer.MLTraceBuffer.append()
+        :param blinding: [None], [int], or [tuple of int] number of samples on the left and
+                            right end of appended traces to "blind" (i.e. set fold = 0)
+                            ~also see wyrm.data.mltrace.MLTrace.apply_blinding() for specific behviors
+        ~~~Buffering Append Behavior Influencing Attributes~~~
+        :param method: [int] or [str] - method for wyrm.data.mltrace.MLTrace.__add__
+                        Supported:
+                            0, 'dis','discard' - discard overlapping dat
+                            1, 'int','interpolate' - interploate across overlapping data
+                            2, 'max', 'maximum' - conduct stacking with "max" behavior
+                            3, 'avg', 'average' - condcut stacking with "avg" behavior
+                        ~also see wyr.data.mltrace.MLTrace.__add__()
+        :param **add_kwargs: [kwargs] key-word argument collector passed to MLTrace.__add__() to alter
+                            behaviors of calls of MLTraceBuffer.append() made in the BufferWyrm.pulse() method
+                            ~also see wyrm.data.mltrace.MLTrace.__add__()
+                            ~also see wyrm.data.mltracebuffer.MLTraceBuffer.append()
+
         :param max_pulse_size: [int] maximum number of items in `x` to assess in a single call of BufferWyrm.pulse(x)
         :param debug: [bool] - run in debug mode?
-        :param **add_kwargs: [kwargs] key-word argument collector for passing to MLTraceBuffer.__add__() to alter
-                        default parameter value(s) for all calls of MLTraceBuffer.__add__ conducted by 
-                        __init__ or pulse()
+
         """
-        self.max_length = bounded_floatlike(
-            max_length,
-            name='max_length',
-            minimum = 0,
-            maximum=None,
-            inclusive=False
-            )
-
         super().__init__(max_pulse_size=max_pulse_size, debug=debug)
-
-        if add_method in [0,'dis','discard',
-                          1,'int','interpolate',
-                          2,'max','maximum',
-                          3,'avg','average']:
-            self.add_method = add_method
-        else:
-            raise ValueError(f'add_method {add_method} not supported. See wyrm.data.mltrace.MLTrace.__add__()')
-        
+        # Initialize Buffer Containing Object
+        self.buffer = DictStream(key_attr=buffer_key)
+        # Create holder attribute for MLTraceBuffer initialization Kwargs
+        self.mltb_kwargs = {}
+        # Do sanity checks on max_length
+        self.mltb_kwargs.update({'max_length':
+                                 bounded_floatlike(
+                                        max_length,
+                                        name='max_length',
+                                        minimum = 0,
+                                        maximum=None,
+                                        inclusive=False
+                                        )})
+        # Do sanity checks on restrict_past_append
         if not isinstance(restrict_past_append, bool):
             raise TypeError('restrict_past_append must be type bool')
         else:
-            self.restrict_past_append = restrict_past_append
-
+            self.mltb_kwargs.update({'restrict_past_append': restrict_past_append})
+        # Do sanity checks on blinding
         if not isinstance(blinding, (type(None), bool, tuple)):
             raise TypeError
         else:
-            self.blinding = blinding
-
-   
-        
-            
-
-        # _stack_kwargs = {'blinding_samples': int,'method': str,'fill_value': (type(None), int, float),'sanity_checks': bool}
-        # _merge_kwargs = {'interpolation_samples': int,'method': str,'fill_value': (type(None), int, float),'sanity_checks': bool}
-        # _kwarg_checks = {'stack': _stack_kwargs, 'merge': _merge_kwargs}
-
-        # for _k, _v in add_kwargs.items():
-        #     if _k not in _kwarg_checks[self.add_style].keys():
-        #         raise KeyError(f'kwarg {_k} is not supported for add_style "{self.add_style}"')
-        #     elif not isinstance(_v, _kwarg_checks[self.add_style][_k]):
-        #         raise TypeError(f'kwarg {_k} type "{type(_v)}" not supported for add_style "{self.add_style}')
+            self.mltb_kwargs({'blinding': blinding})
+        # Create holder for add_kwargs to pass to 
         self.add_kwargs = add_kwargs
-            
-        if not isinstance(restrict_add_past, bool):
-            raise TypeError
+        if method in [0,'dis','discard',
+                          1,'int','interpolate',
+                          2,'max','maximum',
+                          3,'avg','average']:
+            self.add_kwargs.update({'method': method})
         else:
-            self.restrict_add_past = restrict_add_past
-        # Initialize Buffer Containing Object
-        self.buffer = DictStream()
+            raise ValueError(f'add_method {method} not supported. See wyrm.data.mltrace.MLTrace.__add__()')
+        
+
+
+    def add_new_buffer(self, other):
+        if isinstance(other, Trace):
+            if other.id not in self.buffer.traces.keys():
+                # Initialize an MLTraceBuffer Object
+                mltb = MLTraceBuffer(**self.mltb_kwargs,
+                                     **self.add_kwargs)
+                mltb.append(other)
+                self.buffer.traces.update({mltb.id: mltb})
+            
 
     def pulse(self, x):
         """
         Conduct a pulse on input deque of MLTrace objects (x)
         """
+        # Kick error if input is not collections.deque object
         if not isinstance(x, deque):
             raise TypeError
-        
+        # Get initial deque length
         qlen = len(x)
+        # Iterate up to max_pulse_size times
         for _i in range(self.max_pulse_size):
+            # Early stopping if no items in queue
             if qlen == 0:
                 break
+            # Early stopping if next iteration would exceed
+            # the number of items in queue
             elif _i + 1 > qlen:
                 break
+            # otherwise, popleft to get oldest item in queue
             else:
                 _x = x.popleft()
-            
+            # if not an MLTrace (or child) reappend to queue (safety catch)
             if not isinstance(_x, MLTrace):
                 x.append(_x)
+            #Otherwise get ID of MLTrace
             else:
                 _id = _x.id
+                if _id not in self.buffer.traces.keys():
+                    self.add_mltracebuffer(self.)
                 if _id not in self.buffer.labels():
                     new_buffer_item = MLTraceBuffer(max_length=self.max_length,
                                                add_style=self.add_style,

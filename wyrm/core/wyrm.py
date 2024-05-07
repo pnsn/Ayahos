3bdbf6d3f6ca8fc8ba4a26d0f776b8e981e52709
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2024-05-07 15:58:52 -0700

    Fiddle with Sphinx-compliant docstrings

--- wyrm/core/wyrm.py
+++ wyrm/core/wyrm.py
@@ -10,59 +10,39 @@
     other *Wyrm classes -- "Wyrm" -- and serves as a template
     for the minimum required methods of each successor class. 
 """
-import wyrm.util.input as wcc
 from copy import deepcopy
 from time import time
 import logging
 
-logger = logging.getLogger(__name__)
+Logger = logging.getLogger(__name__)
 
-class Wyrm:
-    """
-    Fundamental Base Class for all *Wyrm classes in this module that are
-    defined by having the y = *wyrm.pulse(x) class method.
+class Wyrm(object):
+    """Fundamental base class for all other *Wyrm classes
 
-    The Wyrm base class produces an object with no attributes and placeholders
-    for fundamental class-methods common to all Wyrm class objects:
-
-    + __init__
-    + __str__
-    + __repr__
-    + pulse
-
-    And attributes:
-    @ max_pulse_size - reference integer for maximum number of iterations for
-                        the outer loop of a given pulse() method
-    @ debug - bool switch for running the wyrm in debug mode (in development)
+    :return: Wyrm object
+    :rtype: wyrm.core.wyrm.Wyrm
     """
 
-    def __init__(self, timestamp=False, max_pulse_size=None, debug=False):
-        """
-        Initialize a Wyrm object
-        """
-        self.logger = logging.getLogger(__name__)
-        self.logger.info('creating an instance of Wyrm')
-        # Compatability check for debug
-        if not isinstance(debug, bool):
-            raise TypeError("debug must be type bool")
-        else:
-            self.debug = debug
-        
-        if isinstance(timestamp, bool):
-            self._timestamp = timestamp
-        else:
-            raise TypeError('timestamp must be type bool')
+    def __init__(self, max_pulse_size=None):
+        """Initialize a Wyrm object
 
+        :param max_pulse_size: maximum , defaults to None
+        :type max_pulse_size: _type_, optional
+        :raises ValueError: for non-positive number argument for max_pulse_size
+        :raises TypeError: for non-int-like or non-NoneType argument for max_pulse_size
+        """
+        Logger.debug('Initializing a Wyrm object')
         # Compatability check for max_pulse_size
         if max_pulse_size is None:
             self.max_pulse_size = None
         elif isinstance(max_pulse_size, int, float):
-            if 1 <= max_pulse_size <= 1e6:
+            if 1 <= max_pulse_size:
                 self.max_pulse_size = int(max_pulse_size)
             else:
-                raise ValueError('max_pulse_size must be between 1 and 1000000')
+                raise ValueError('max_pulse_size must be g.e. 1 ')
         else:
             raise TypeError('max_pulse_size must be NoneType, or positive int-like')
+
 
     def __repr__(self):
         """
@@ -86,15 +66,14 @@ class Wyrm:
         return deepcopy(self)
 
     def pulse(self, x=None):
-        """
-        Run a pulse with input argument and return that argument
-        with check that input x is the expected _in_type
-        :: INPUT ::
-        :param x: [type] or [NoneType] input object x
-        :param options: [kwargs] collector for addtional key word arguments
-                        to pass to internal processes
-        :: OUTPUT ::
-        :return y: [type] or [NoneType] alias of input x
-        """
+        """Run a pulse where the input equals the output
+
+        :param x: input value, defaults to None
+        :type x: any, optional
+        :return: output value
+        :rtype: same as x
+        """        
+        Logger.debug('initiating pulse')
         y = x
+        Logger.debug('concluding pulse')
         return y

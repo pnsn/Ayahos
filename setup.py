5d1af8fe606f99b53d11dfb7cc559fc3d2af9354
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2024-05-09 15:59:40 -0700

    Try copy over from seisbench setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""
+Wyrm - a python project splicing python machine learning seismology tools into the Earthworm Message Transport System
+
+Wyrm is a open-source project that builds on the PyEarthworm, ObsPy, Numpy, and SeisBench APIs to facilitate
+testing and operationalization of Python-based machine learning workflows that use Earthworm messages as inputs
+and outputs
+
+:copyright: Nathan T. Stevens and Pacific Northwest Seismic Network (pnsn.org)
+:license: AGPL-3.0
+"""
+from setuptools import setup
+
+setup()

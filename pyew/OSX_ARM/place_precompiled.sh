c7878b06d1c5c6f94091919cddf3cc2794534565
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2024-05-09 07:59:19 -0700

    Shift precompile to ignored

--- pyew/OSX_ARM/place_precompiled.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-ENV_ROOT=~/miniconda3/envs/wyrm_v0
-cp PyEW.cpython-312-darwin.so $ENV_ROOT/lib/python3.12/site-packages/
-cp -r PyEarthworm-1.41.dist-info $ENV_ROOT/lib/python3.12/site-packages/

8967b81c54c411d697dd9f1d8e3390fcfe21ffc4
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2024-05-14 10:41:40 -0700

    Update methods for new pulse paradigm

--- ayahos/core/wyrms/ringwyrm.py
+++ ayahos/core/wyrms/ringwyrm.py
@@ -23,6 +23,7 @@
 """
 import logging
 import PyEW
+from collections import deque
 from ayahos.core.wyrms.wyrm import Wyrm
 from ayahos.util.pyew import is_wave_msg, wave2mltrace
 from ayahos.util.input import bounded_floatlike, bounded_intlike
@@ -81,95 +82,170 @@ class RingWyrm(Wyrm):
         if isinstance(module, PyEW.EWModule):
             self.module = module
         else:
-            raise TypeError(f'module must be ')
-        if module is None:
-            self.module = module
-            print('No EW connection provided - for debugging/dev purposes only')
-        elif not isinstance(module, PyEW.EWModule):
-            raise TypeError('module must be a PyEW.EWModule object')
+            raise TypeError(f'module must be type PyEW.EWModule, not type {type(module)}')
+        
+        if isinstance(conn_id, int):
+            if conn_id > 0:
+                self.conn_id = conn_id
+            else:
+                raise ValueError('conn_id must be a non-negative iteger')
         else:
-            self.module = module
+            raise TypeError('conn_id must be type int')
 
-        # Compatability checks for `conn_id`
-        self.conn_id = self._bounded_intlike_check(conn_id, name='conn_id', minimum=0)
-
-        # Compat. chekcs for pulse_method
+        # Compatability or pulse_method
         if pulse_method not in ['get_wave','put_wave','get_msg','put_msg','get_bytes','put_bytes']:
             raise ValueError(f'pulse_method {pulse_method} unsupported. See documentation')
         else:
             self.pulse_method = pulse_method
         
         # Compatability checks for msg_type
-        if self.pulse_method in ['get_msg','put_msg','get_bytes','put_bytes']:
-            self.msg_type = bounded_intlike(
-                msg_type,
-                name='msg_type',
-                minimum=0,
-                maximum=255,
-                inclusive=True
-            )
-        # In the case of get/put_wave, default to msg_type=19 (tracebuff2)
-        else:
-            self.msg_type=19
+        if isinstance(msg_type, int):
+            if self.pulse_method in ['get_msg','put_msg','get_bytes','put_bytes']:
+                if 0 <= msg_type <= 255:
+                    self.msg_type = msg_type
+                else:
+                    raise ValueError(f'msg_type value {msg_type} out of bounds [0, 255]')
+            # Hard set TYPE_TRACEBUFF2 for get/put_wave
+            else:
+                self.msg_type = 19
+        self._core_args = [self.conn_id, self.msg_type]
+
         Logger.info('Ringwyrm method {0} for message type {1}'.format(self.pulse_method, self.msg_type))
 
+    def _core_process(self, x):
+        """_core_process for ayahos.core.wyrms.ringwyrm.RingWyrm
+
+        Conduct a single transaction based on the following RingWyrm attributes
+        .pulse_method
+        .msg_type
+        .conn_id
+
+        :param x: for 'put_*' methods - collection of message-like objects to submit to ring 
+                  for 'get_*' methods - None
+        :type x: collections.deque or None
+        :return y: for 'put_*' methods - None
+                   for 'get_*' methods - collection of message objects
+        :rtype: None or collections.deque
+        """        
+        if 'put' in self.pulse_method:
+            if not isinstance(x, deque):
+                raise 
+            _x = x.popleft()
+            _y = getattr(self.module, self.pulse_method)(*self._core_args, _x)
+        elif 'get' in self.pulse_method:
+            _y = getattr(self.module, self.pulse_method)(*self._core_args)
+        return _y
+
+    def _get_early_stopping(self, _y):
+
+        if self.pulse_method == 'get_msg':
+            if _y == '':
+                return True
+            else:
+                return False
+        if self.pulse_method == 'get_bytes':
+            if _y == (0,0):
+                return True
+            else:
+                return False
+        if self.pulse_method == 'get_wave':
+            if _y == {}:
+                return True
+            else:
+                return False
+
+    def _put_early_stopping(self, x, i_):
+        """Use _early_stopping from ayahos.core.wyrms.wyrm.Wyrm for "put" type pulse methods
+
+        Early stopping if iteration counter exceeds length of `x`
+        Early stopping if length of `x` is 0
+
+        :param x: collection of messages being submitted
+        :type x: collections.deque
+        :param i_: iteration counter
+        :type i_: int
+        :return: status
+                True = Trigger early stopping
+                False = do not trigger early stopping
+        :rtype: bool
+        """        
+        status = super()._early_stopping(x, i_)
+        return status
+
     def pulse(self, x):
-        """
-        Conduct a single transaction between an Earthworm ring
-        and the Python instance this RingWyrm is operating in
-        using the PyEW.EWModule.get/put -type method and msg_type
-        assigned to this RingWyrm
-
-        :: INPUT ::
-        :param x: for 'put' type pulse_method_str instances of RingWyrm
-                this is a message object formatted to submit to target 
-                Earthworm ring following PyEW formatting guidelines
-                see
-        :: OUTPUT ::
-        :return msg: for 'get' type pulse_method_str instances of RingWyrm
-                this is a message object produced by a single call of the
-                PyEW.EWModule.get_... method specified when the RingWyrm
-                was initialized. 
-                
-                NOTE: Empty messages return False to signal no new messages 
-                were available of specified msg_type in target ring conn_id.
-
-                for get_wave() - returns a dictionary
-                for get_bytes() - returns a python bytestring
-                for get_msg() - returns a python string
-        """
-        # If getting things from Earthworm...
         if 'get' in self.pulse_method:
-            # ...if getting a TYPE_TRACEBUFF2 (code 19) message, use class method directly
-            if 'wave' in self.pulse_method:
-                msg = self.module.get_wave(self.conn_id)
-                # Flag null result as False
-                if msg == {}:
-                    msg = False
-            # ...if getting a string or bytestring, use eval approach for compact code
-            else:
-                eval_str = f'self.module.{self.pulse_method}(self.conn_id, self.msg_type)'
-                msg = eval(eval_str)
-                # Flag empty message results as 'False'
-                if msg == '':
-                    msg = False
-            
-            return msg
-        
-        # If sending things to Earthworm...
+            for i_ in range(self.max_pulse_size):
+                _y = self._core_process(x)
+                self.output.append(_y)
+                if self._get_early_stopping(x):
+                    break
+            y = self.output
         elif 'put' in self.pulse_method:
-            msg = x
-            # ..if sending waves
-            if 'wave' in self.pulse_method and is_wave_msg(msg):
-                # Commit to ring
-                self.module.put_wave(self.conn_id, msg)
-            # If sending byte or string messages
-            else:
-                # Compose evalstr
-                eval_str = f'self.module.{self.pulse_method}(self.conn_id, self.msg_type, x)'
-                # Execute eval
-                eval(eval_str)
-            return None      
+            for i_ in range(self.max_pulse_size):
+                if self._put_early_stopping(x, i_):
+                    break
+                self._core_process(x)
+            y = None
+        return y
+
+
+    # def pulse(self, x):
+    #     """
+    #     Conduct a single transaction between an Earthworm ring
+    #     and the Python instance this RingWyrm is operating in
+    #     using the PyEW.EWModule.get/put -type method and msg_type
+    #     assigned to this RingWyrm
+
+    #     :: INPUT ::
+    #     :param x: for 'put' type pulse_method_str instances of RingWyrm
+    #             this is a message object formatted to submit to target 
+    #             Earthworm ring following PyEW formatting guidelines
+    #             see
+    #     :: OUTPUT ::
+    #     :return msg: for 'get' type pulse_method_str instances of RingWyrm
+    #             this is a message object produced by a single call of the
+    #             PyEW.EWModule.get_... method specified when the RingWyrm
+    #             was initialized. 
+                
+    #             NOTE: Empty messages return False to signal no new messages 
+    #             were available of specified msg_type in target ring conn_id.
+
+    #             for get_wave() - returns a dictionary
+    #             for get_bytes() - returns a python bytestring
+    #             for get_msg() - returns a python string
+    #     """
+    #     # If getting things from Earthworm...
+    #     if 'get' in self.pulse_method:
+    #         # ...if getting a TYPE_TRACEBUFF2 (code 19) message, use class method directly
+    #         if 'wave' in self.pulse_method:
+    #             msg = self.module.get_wave(self.conn_id)
+    #             # Flag null result as False
+    #             if msg == {}:
+    #                 msg = False
+    #         # ...if getting a string or bytestring, use eval approach for compact code
+    #         else:
+    #             eval_str = f'self.module.{self.pulse_method}(self.conn_id, self.msg_type)'
+    #             msg = eval(eval_str)
+    #             # Flag empty message results as 'False'
+    #             if msg == '':
+    #                 msg = False
+            
+    #         return msg
+        
+    #     # If sending things to Earthworm...
+    #     elif 'put' in self.pulse_method:
+    #         msg = x
+    #         # ..if sending waves
+    #         if 'wave' in self.pulse_method and is_wave_msg(msg):
+    #             # Commit to ring
+    #             self.module.put_wave(self.conn_id, msg)
+    #         # If sending byte or string messages
+    #         else:
+    #             # Compose evalstr
+    #             eval_str = f'self.module.{self.pulse_method}(self.conn_id, self.msg_type, x)'
+    #             # Execute eval
+    #             eval(eval_str)
+    #         return None      
 
     def __repr__(self):
         """
@@ -186,8 +262,8 @@ class RingWyrm(Wyrm):
         return rstr
     
     def __str__(self):
-        rstr = f'wyrm.wyrms.io.RingWyrm(module={self.module}, '
-        rstr += f'conn_id={self.conn_id}, pulse_method_str={self.pulse_method}, '
+        rstr = f'ayahos.core.wyrms.ringwyrm.RingWyrm(module={self.module}, '
+        rstr += f'conn_id={self.conn_id}, pulse_method={self.pulse_method}, '
         rstr += f'msg_type={self.msg_type}, '
-        rstr += f'max_pulse_size={self.max_pulse_size}, debug={self.debug})'
+        rstr += f'max_pulse_size={self.max_pulse_size})'
         return rstr

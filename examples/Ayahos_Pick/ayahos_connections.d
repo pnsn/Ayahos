277575f30e9bafe1dfa56d2866f40f1a3fb96b69
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2024-05-17 11:32:39 -0700

    Continue example develoment

--- examples/Ayahos_Pick/ayahos_connections.d
+++ examples/Ayahos_Pick/ayahos_connections.d
@@ -10,12 +10,24 @@ DEFAULT_RING_ID: ${Common: DEFAULT_RING}
 MODULE_ID: 200
 INSTALLATION_ID: ${Common: INST_ID}
 HEARTBEAT_PERIOD: 30
-conn_dict: {"PICK":%(Common: PICK_RING), "TMP":1010}
+CONN_DICT: {"PICK":%(Common: PICK_RING), "TMP":1010}
+WAIT_SEC: 0.
 
 [iringwyrm]
-CLASS: ayahos.core.wyrms.ringwyrm.ringwyrm
+CLASS: ayahos.core.wyrms.ringwyrm.RingWyrm
+MODULE: ayahos.module
+CONN_ID: 0
+PULSE_METHOD: get_wave
+MSG_TYPE: 19
+MAX_PULSE_SIZE: 1000
 
-
+[oringwyrm]
+CLASS: ayahos.core.wyrms.ringwyrm.RingWyrm
+MODULE: ayahos.module
+CONN_ID: 1
+PULSE_METHOD: put_wave
+MSG_TYPE: 19
+MAX_PULSE_SIZE: 1000
 
 
 [wyrm_dict]

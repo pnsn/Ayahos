1e44f215f2bd57ba30e315c0f7aa87e64815ca23
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2024-05-23 16:34:13 -0700

    Update polymorphic subroutines

--- ayahos/wyrms/wyrm.py
+++ ayahos/wyrms/wyrm.py
@@ -31,11 +31,18 @@ class Wyrm(object):
     """Fundamental base class template all other *Wyrm classes with the following
     template methods
 
-    Wyrm().pulse() - performs a for-loop execution of many Wyrm().core_process() calls
-    Wyrm().core_process() - template method for polymorphic inheritance
+    Wyrm().pulse() - performs multiple iterations of the following polymorphic subroutines
+        Wyrm()._run_this_iteration() - check if this iteration in pulse should be run
+        Wyrm()._unit_input_from_input() - get the object input for _unit_process()
+        Wyrm()._unit_process() - execute a single operation on the input object
+        Wyrm()._capture_unit_output() - do something to save the output of _unit_process
+        Wyrm()._run_next_iteration() - decide if the next iteration should be run
 
     Wyrm.output - collector of outputs from core_process(). Should be modified along with
                 Wyrm().core_process() to meet your needs.
+
+    :param max_pulse_size: maximum number of iterations to run for wyrm.pulse(), defaults to 10
+    :type max_pulse_size: int, optional
     
     """
 
@@ -90,14 +97,15 @@ class Wyrm(object):
         for ayahos.core.wyrms.{class_name_lower}.{class_name_camel}
 
         NOTE: Houses the following polymorphic methods
-            _continue_iteration: check if iteration should continue
-            _get_obj_from_input: get input object for _unit_process
+            _run_this_iteration: check if iteration should continue (early stopping opportunity)
+            _unit_input_from_input: get input object for _unit_process
             _unit_process: run core process
             _capture_output: attach _unit_process output to {class_name_camel}.output
+            _run_next_iteration: check if the next iteration should occur (early stopping opportunity)
 
         :param input: standard input
         :type input: collections.deque
-            see ayahos.core.wyrms.wyrm.Wyrm._get_obj_from_input()
+            see ayahos.core.wyrms.wyrm.Wyrm._unit_input_from_input()
         :return output: aliased access to {class_name_camel}.output
         :rtype output: collections.deque of objects
         """ 
@@ -107,31 +115,38 @@ class Wyrm(object):
         nproc = 0
         for iterno in range(self.max_pulse_size):
             # Check if this iteration should proceed
-            status1 = self._continue_iteration(input, input_measure, iterno)
+            run_this_iteration = self._run_this_iteration(input, input_measure, iterno)
             # If iterations should continue
-            if status1:
-                # get single object for unit_process
-                obj = self._get_obj_from_input(input)
-                # Execute unit process
-                unit_out = self._unit_process(obj)
-                nproc += 1
-                # Capture output and check if early stopping should occur at the end of this iteration
-                try:
-                    status2 = self._capture_unit_out(unit_out)
-                except:
-                    breakpoint()
-                if status2 is False:
-                    break
-            # If iterations should not continue
-            else:
-                # end iterations
+            if run_this_iteration is False:
                 break
+            else:
+                pass
+            # get single object for unit_process
+            unit_input = self._unit_input_from_input(input)
+            # Execute unit process
+            unit_output = self._unit_process(unit_input)
+            nproc += 1
+            # Capture output
+            self._capture_unit_output(unit_output)
+            #  Check if early stopping should occur at the end of this iteration
+            run_next_iteration = self._run_next_iteration(unit_output)
+            if run_next_iteration is False:
+                break
+            else:
+                pass
         # Get alias of self.output as output
         output = self.output
         return output, nproc
 
     def _measure_input(self, input):
-        """reference measurement for input
+        """
+        POLYMORPHIC - last inheritance :class: `~ayahos.wyrms.wyrm.Wyrm`
+
+        take a reference measurement for the input before starting
+        iterations for :meth: `~ayahos.core.wyrm.Wyrm.pulse(input)` prior
+        to starting the pulse.
+
+        This version measures the length of input.
 
         :param input: standard input
         :type input: varies, deque here
@@ -144,10 +159,12 @@ class Wyrm(object):
             input_measure = len(input)
         return input_measure
 
-    def _continue_iteration(self, input, input_measure, iterno):
-        """Iteration continuation criteria for {class_name_camel}
-        POLYMORPHIC
+    def _run_this_iteration(self, input, input_measure, iterno):
+        """
+        POLYMORPHIC - last inheritance :class: `~ayahos.wyrms.wyrm.Wyrm`
 
+        Should this iteration in :meth: `~ayahos.wyrms.wyrm.Wyrm.pulse()` be run?
+        
         Criteria:
          - input is type collections.deque
          - len(input) > 0
@@ -170,9 +187,10 @@ class Wyrm(object):
                     status = True
         return status
     
-    def _get_obj_from_input(self, input):
-        """_get_obj_from_input for Wyrm
-        POLYMORPHIC
+    def _unit_input_from_input(self, input):
+        """
+        POLYMORPHIC - last inheritance :class: `~ayahos.wyrms.wyrm.Wyrm`
+
 
         Get the input object for this Wyrm's _unit_process
 
@@ -192,11 +210,11 @@ class Wyrm(object):
             raise TypeError
         
 
-    def _unit_process(self, obj):
-        """unit_process for ayahos.core.wyrms.wyrm.Wyrm
-        POLYMORPHIC
+    def _unit_process(self, unit_input):
+        """
+        POLYMORPHIC - last inheritance :class: `~ayahos.wyrms.wyrm.Wyrm`
 
-        return unit_out = obj
+        return unit_output = unit_input
 
         :param obj: input object
         :type obj: any
@@ -206,15 +224,31 @@ class Wyrm(object):
         unit_out = obj
         return unit_out        
     
-    def _capture_unit_out(self, unit_out):
-        """Append the unit_out to Wyrm().output
-        POLYMORPHIC
+    def _capture_unit_output(self, unit_output):
+        """
+        POLYMORPHIC - last inheritance :class: `~ayahos.wyrms.wyrm.Wyrm`
+
+        Append unit_output to self.output
 
         run Wyrm().output.append(unit_out)
 
         :param unit_out: standard output object from unit_process
         :type unit_out: any
         """        
-        self.output.append(unit_out)
+        self.output.append(unit_output)
+        
+    def _run_next_iteration(self, unit_output):
+        """
+        POLYMORPHIC - last inheritance :class: `~ayahos.wyrms.wyrm.Wyrm`
+
+        check if the next iteration should be run based on the unit_output
+
+        Returns status = True unconditionally
+
+        :param unit_out: output _unit_process
+        :type unit_out: object
+        :return status: should the next iteration be run?
+        :rtype: bool
+        """
         status = True
         return status

e5d8f957d2a5eb7b40591124390c31d51d1090ba
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2024-06-11 13:03:40 -0700

    Initial-ish commit

---
+++ setup_env.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# :auth: Nathan T. Stevens
+# :email: ntsteven (at) uw.edu
+# :org: Pacific Northwest Seismic Network
+# :license: AGPL-3.0
+# :purpose: This script creates a conda environment and installs Ayahos and all its dependencies
+ENVNAME='Ayahos_Auto'
+ACTIVATE="$CONDA_PREFIX/bin/activate"
+
+# Check if environment exists
+if conda info --envs | grep -q "$ENVNAME"; then
+    echo "Conda env '$ENVNAME' already exists"
+# If it doesn't exist, create environment and install dependencies
+else
+    conda create --name $ENVNAME python=3.9 -y
+    echo "~~~~~~~ Environment '$ENVNAME' created ~~~~~~~"
+    source "$ACTIVATE" "$ENVNAME"
+fi
+
+#  Check if environment is active
+if [[ "$CONDA_DEFAULT_ENV" == "$ENVNAME" ]]; then
+    echo "~~~~~~~ Environment '$ENVNAME' already active ~~~~~~~"    
+else
+    echo "~~~~~~~ Activating '$ENVNAME' ~~~~~~"
+    source "$ACTIVATE" "$ENVNAME"
+fi
+
+# Install Ayahos and dependencies using pip backend
+if [[ "$CONDA_DEFAULT_ENV" == "$ENVNAME" ]]; then
+    echo "~~~~~~~ Installing Ayahos and dependencies from local copy ~~~~~~~"
+    python -m pip install .
+fi

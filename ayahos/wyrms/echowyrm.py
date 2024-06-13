9dee172474bb66222aa12c23a86870c613e538af
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2024-06-12 23:39:16 -0700

    update polymorphics

--- ayahos/wyrms/echowyrm.py
+++ ayahos/wyrms/echowyrm.py
@@ -5,10 +5,17 @@ Logger = logging.getLogger(__name__)
 
 class EchoWyrm(Wyrm):
 
-    def __init__(self, max_pulse_size=10000, mute_pulse_logging=True):
-        super().__init__(max_pulse_size=max_pulse_size, mute_pulse_logging=mute_pulse_logging)
+    def __init__(self, max_pulse_size=10, max_output_size=100, meta_memory=3600, report_period=False):
+        super().__init__(max_pulse_size=max_pulse_size,
+                         max_output_size=max_output_size,
+                         meta_memory=meta_memory,
+                         report_period=report_period)
+
+
     
-    def _unit_process(self, unit_input):
-        Logger.info(f'{unit_input}')
-        unit_output = unit_input
-        return unit_output
+    def _capture_unit_output(self, unit_output):
+        self.output.append(unit_output)
+        while len(self.output) > self.max_output_size:
+            obj  = self.output.popleft()
+            Logger.info(f'\n\n{obj}\n')
+            self.output.popleft()

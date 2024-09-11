8064b877729cd2174cbef1c8c95d9a06f3895b90
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2024-09-11 14:51:43 -0700

    Have bysource order to class methods

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,7 +40,7 @@ extensions = [
 
 templates_path = ['_templates']
 exclude_patterns = []
-
+autodoc_member_order = 'bysource'
 
 
 # -- Options for HTML output -------------------------------------------------

649defefe4681b3a63e7dc642fd38873e294f373
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2024-09-30 14:44:31 -0700

    Update several

--- PULSE/data/foldtrace.py
+++ PULSE/data/foldtrace.py
@@ -1,12 +1,28 @@
-import decorator
+
 import numpy as np
+from obspy.core.stream import Stream
 from obspy.core.utcdatetime import UTCDateTime
 from obspy.core.trace import Trace, Stats
 from PULSE.data.header import MLStats
+from obspy.core.util.misc import flat_not_masked_contiguous
 
 class FoldTrace(Trace):
 
-    def __init__(self, data=np.array([]), fold=None, header=None, dtype=None):
+    def __init__(self, data=np.array([]), fold=None, header=None, dtype=np.float32):
+        """Create a :class:`~PULSE.data.foldtrace.FoldTrace` object
+
+        :param data: Input data array, defaults to np.array([])
+            Note: will also accept a :class:`~obspy.core.trace.Trace` object
+        :type data: numpy.ndarray or Trace, optional
+        :param fold: fold vector of equal size as data, defaults to None
+            None results in fold values of 1 for all data, except masked values
+            which have a fold value of 0
+        :type fold: None or numpy.ndarray, optional
+        :param header: non-default metadata values, defaults to None
+        :type header: None or dict, optional
+        :param dtype: default data type to use for **data** and **fold**, defaults to np.float32
+        :type dtype: type, optional
+        """        
         # Handle case where a Trace is directly passed as an arg
         if isinstance(data, Trace):
             trace = data
@@ -30,7 +46,7 @@ class FoldTrace(Trace):
     # ADDED METHODS #
     #################
 
-    def get_view(self, starttime=None, endtime=None):
+    def get_view(self, starttime=None, endtime=None, minfold=0):
         """Create a new FoldTrace object that houses subset views
         of this FoldTrace's **data** and **fold** attributes and
         a deepcopy of this FoldTrace's **stats** with updated 
@@ -48,17 +64,24 @@ class FoldTrace(Trace):
 
         
         :param starttime: starttime of view, defaults to None
-        :type starttime: None or UTCDateTime, optional
+        :type starttime: None or :class:`~obspy.core.utcdatetime.UTCDateTime`, optional
         :param endtime: endtime of view, defaults to None
-        :type endtime: _type_, optional
-        :return: _description_
-        :rtype: _type_
+        :type endtime: None or :class:`~obspy.core.utcdatetime.UTCDateTime`, optional
+        :return:
+         - **ftr** (*PULSE.data.foldtrace.FoldTrace**) -- FoldTrace containing the subset
+            **data** and **fold** views with an updated copy of metadata in **stats**
         """        
         # Get starting and ending indices
-        ii = self.stats.utc2nearest_index(starttime,ref='starttime')
+        if starttime is None:
+            ii = 0
+        else:
+            ii = self.stats.utc2nearest_index(starttime,ref='starttime')
         if ii < 0:
             ii = 0
-        ff = self.stats.utc2nearest_index(endtime,ref='endtime')
+        if endtime is None:
+            ff = self.stats.npts - 1
+        else:
+            ff = self.stats.utc2nearest_index(endtime,ref='endtime')
         if ff > self.stats.npts:
             ff = self.stats.npts - 1
         # Create deep copy of header
@@ -96,7 +119,7 @@ class FoldTrace(Trace):
         options.update({'starttime': starttime, 'endtime': endtime})
         ftr._apply_to_data_and_fold('trim', **options)
         return ftr
-    
+
     def extend(self, other, **options):
         """Extend this FoldTrace with a Trace-like object 
         If **other** is an ObsPy trace, it is first converted into a FoldTrace and subsequently added.
@@ -106,7 +129,18 @@ class FoldTrace(Trace):
         """
         if not isinstance(other, FoldTrace):
             other = FoldTrace(other)
-        self.apply_inplace_method_to_masked('__add__', other, process_fold=True, **options)
+        self.apply_inplace_method_to_masked('__add__', other, process_fold=False, **options)
+
+    def stack(self, other, method='avg', **options):
+        """Stack two fold traces using a range of operators that are fold-aware
+
+        :param other: _description_
+        :type other: _type_
+        :param method: _description_, defaults to 'avg'
+        :type method: str, optional
+        :raises NotImplementedError: _description_
+        """        
+        raise NotImplementedError('Work in progress')
 
     def apply_inplace_method_to_masked(self, method, *args, process_fold=True, **kwargs):
         """Conduct a dynamic call of a method from :class:`~obspy.core.trace.Trace`
@@ -146,6 +180,189 @@ class FoldTrace(Trace):
         # Otherwise use the method as normal
         else:
             self._apply_inplace_method(method, *args, process_fold=process_fold, **kwargs)
+
+    ###################
+    # SPECIAL METHODS #
+    ###################
+
+    def __add__(self, other, method=0, fill_value=None):
+        """Add another Trace-like object to this FoldTrace
+        Overwrites the functionalities of :meth:`~obspy.core.trace.Trace.__add__`
+            
+        :param other: FoldTrace object
+        :type other: :class:`~PULSE.data.foldtrace.FoldTrace`
+        :param method: method to apply, see above, defaults to 0
+        :type method: int, optional
+        :param fill_value: fill value passed to numpy.ma.MaskedArray if there are
+            masked values, defaults to None
+        :type fill_value: scalar, optional
+
+        .. rubric:: Notable differences from Trace.__add__
+         - Requires **other** to pass sanity checks
+         - Updates **fold** attribute
+         - Conducts an in-place modification to self
+         - Fill value only populates the fill_value attribute of **data**
+         if it is a :class:`~numpy.ma.MaskedArray`
+         - method = 1 is not supported
+         
+        .. rubric:: Supported Methods
+
+            All methods - gaps produce samples with masked data and 0-valued fold samples
+
+            method = 0: drop overlapping samples
+             - Drops overlapping data samples (become masked values)
+             - thus overlapping data -> 0 fold
+             - follows behavior method=0 for obspy Trace.__add__
+            
+            method = 1: simple interpolation for overlapping samples
+             - Not supported - raises NotImplementedError
+
+            method = 2: maximum-value stacking
+             - Larger value of overlapping data samples is retained
+             - fold of samples is added
+
+            method = 3: fold-weighted average stacking
+             - Fold-weighted mean of samples is evaluated for overlapping samples
+             - fold of overlapping samples is added
+
+        """        
+        # Run sanity checks
+        if not isinstance(other, FoldTrace):
+            raise TypeError
+        if self.fold.dtype != other.fold.dtype:
+            raise TypeError
+        if self.data.dtype != other.data.dtype:
+            raise TypeError
+        if self.get_id() != other.get_id():
+            raise TypeError
+        if self.stats.sampling_rate != other.stats.sampling_rate:
+            raise TypeError
+        if self.stats.calib != other.stats.calib:
+            raise TypeError
+        
+        # Get relative temporal positions of traces
+        # Self leads other by starttime
+        if self.stats.starttime < other.stats.starttime:
+            lt = self
+            rt = other
+        elif self.stats.startime > other.stats.starttime:
+        # Other leads self by starttime
+            lt = other
+            rt = self
+        # Starttimes match
+        else:
+            # Self leads other by endtime
+            if self.stats.endtime < other.stats.endtime:
+                lt = self
+                rt = other
+            # other leads self by endtime
+            elif self.stats.endtime > other.stats.endtime:
+                lt = other
+                rt = self
+            # starttime and endtime match (and sanity checks pass)
+            else:
+                # If data are identical, sum folds and return
+                if self.data == other.data:
+                    self.fold = np.sum(np.c_[self.fold, other.fold],axis=1)
+                    return
+                # If data are not identical, proceed with merge
+                else:
+                    lt = self
+                    rt = other
+        # Create output holder
+        output = self.__class__(header=lt.stats.copy())
+        # Get relative indices
+        i1 = lt.stats.npts
+        i2 = lt.stats.utc2nearest_index(rt.stats.starttime,ref='starttime')
+        i3 = lt.stats.utc2nearest_index(rt.stats.endtime,ref='starttime')
+        # Create (2, new_npts) data array with default value of NaN
+        data = np.full(shape=(2,i3+1), fill_value=np.nan, dtype=self.data.dtype)
+        add_data = np.full(shape=i3+1, fill_value=np.nan, dtype=self.data.dtype)
+        # Insert data from both FoldTraces
+        data[0,:i1] = lt.data
+        data[1,i2:] = rt.data
+        # Create (2 by new_npts) fold array with default value of 0
+        fold = np.full(shape=data.shape, fill_value=0, dtype=self.data.dtype)
+        add_fold = np.full(shape=add_data.shape, fill_value=0, dtype=self.data.dtype)
+        # Insert fold from both FoldTraces
+        fold[0,:i1] = lt.fold
+        fold[1,i2:] = rt.fold
+        # Find gaps in data
+        gaps = ~np.isfinite(data).any(axis=0)
+        # Find overlaps in data
+        overlaps = np.isfinite(data).all(axis=0)
+        
+        ## PROCESS FOLD IDENTICALLY FOR (almost) ALL METHODS
+        # Find where there are gaps and set fold to 0, sum overlaps
+        add_fold = np.sum(fold, axis=0)
+
+        ## PROCESS DATA - DIFFERENTLY DEPENDING ON METHOD
+        # Method 0) Behavior of Trace.__add__(method=0)
+        if method == 0:
+            # Merge the finite values where there aren't overlaps or gaps
+            add_data[~gaps & ~overlaps] = np.nansum(data, axis=0)
+        # Method 1) Behavior of Trace.__add__(method=1)
+        if method == 1:
+            raise NotImplementedError('ObsPy simple overlap interpolation not used for PULSE')
+
+        # Method 2) Max stacking
+        if method == 2:
+            # Find where therere are gaps and set them to np.nan
+            add_data[~gaps] = np.nansum(data, axis=0)
+
+        # Method 3) Fold-weighted stacking
+        if method == 3:
+            # Set standard values where there are no gaps or overlaps
+            add_data[~gaps & ~overlaps] = np.nansum(data, axis=0)
+            # Find where there are overlaps and use fold to get the weighted average
+            add_data[overlaps] = np.sum(data*fold, axis=0)/add_fold[overlaps]
+        
+        # CLEANUP
+        # Enforce masked array if gaps exist
+        if ~np.isfinite(add_data).all():
+            add_data = np.ma.MaskedArray(data=add_data,
+                                         mask=~np.isfinite(add_data),
+                                         fill_value=fill_value)
+        # Assign data & fold
+        output.data = add_data
+        output.fold = add_fold
+        # Enforce fold rules
+        output._enforce_fold_rules()
+
+        return output
+    
+    def __iadd__(self, other, **options):
+        """_summary_
+
+        :param other: _description_
+        :type other: _type_
+        """        
+        self = self.__add__(other, **options)
+
+    def __eq__(self, other):
+        if not np.array_equal(self.fold, other.fold):
+            return False
+        super().__eq__(self, other)
+
+    def __repr__(self, id_length=None):
+        """Provide a human readable string describing the contents of this
+          :class:`~PULSE.data.foldtrace.FoldTrace` object
+
+        :param id_length: maximum ID length passed to the inherited 
+            :meth:`~obspy.core.trace.Trace.__str__` method, defaults to None
+        :type id_length: None or int, optional
+        :return rstr: representative string
+        :rtype: str
+        """        
+        rstr = super().__str__(id_length=id_length)
+        if self.stats.npts > 0:
+            rstr += f' | Fold:'
+            for _i in range(int(self.fold.max()) + 1):
+                ff = sum(self.fold == _i)/self.stats.npts
+                if ff > 0:
+                    rstr += f' [{_i}] {ff:.2f}'
+        return rstr
+    
 
     ###################################
     # POLYMORPHIC "DATA ONLY" METHODS #
@@ -266,8 +483,47 @@ class FoldTrace(Trace):
             see :meth:`~obspy.core.trace.Trace.resample`
         """
         self.apply_inplace_method_to_masked('resample', sampling_rate, process_fold=True, **options)
-    
-        
+
+    def split(self):
+        """Split a masked FoldTrace into a :class:`~obspy.core.stream.Stream` object
+        containing non-masked, self-contiguous FoldTrace objects
+
+        :raises NotImplementedError: _description_
+        :return:
+         - **output** (*obspy.core.stream.Stream*) -- stream of FoldTrace segments
+            copied from this FoldTrace
+        """        
+        output = Stream()
+        # Data is not a masked array
+        if not isinstance(self.data, np.ma.MaskedArray):
+            output += self.copy()
+        # Data is a masked array
+        else:
+            # No data are masked - convert back to unmasked array
+            if not np.ma.is_masked(self.data):
+                self.data = self.data.data
+                output += self.copy()
+            else:
+                # Generate slices
+                slices = flat_not_masked_contiguous(self.data)
+                output = Stream()
+                for slice in slices:
+                    if slice.step:
+                        raise NotImplementedError("step not supported")
+                    stats = self.stats.copy()
+                    ftr = FoldTrace(header=stats)
+                    ftr.stats.starttime += (stats.delta*slice.start)
+                    ftr.data = self.data.data[slice.start:slice.stop]
+                    ftr.fold = self.fold[slice.start:slice.stop]
+                    output += ftr
+        return output
+
+    def _ltrim(self, starttime, **options):
+        self._apply_inplace_method('_ltrim', starttime, process_fold=True, **options)
+
+    def _rtrim(self, endtime, **options):
+        self._apply_inplace_method('_rtrim', endtime, process_fold=True, **options)
+
 
     #########################
     # ADDED PRIVATE METHODS #
@@ -296,26 +552,9 @@ class FoldTrace(Trace):
         getattr(self, method, *args, **kwargs)
         if process_fold:
             getattr(ftr, method, *args, **kwargs)
-    
-    def _apply_to_data_only(self, method, *args, **kwargs):
-        """Apply a specified :class:`~obspy.core.trace.Trace` class
-        method to only the **data** attributes of this FoldTrace and
-        include handling of "gappy" (i.e., masked) **data** using the
-        :meth:`~PULSE
-        and follow up with :meth:`~PULSE.data.foldtrace.FoldTrace._enforce_fold_rules`
-        
-         
-        :param method: name of the :class:`~obspy.core.trace.Trace` method
-        :type method: _type_
-        """        
-        self.apply_inplace_method_to_masked(method, *args, **kwargs)
-        # Enforce fold rules
-        self._enforce_fold_rules()
 
-    ########################
-    # PRIVATE FOLD METHODS #
-    ########################
-        
+    # FOLD-SPECIFIC PRIVATE METHODS #
+            
     def _enforce_fold_rules(self):
         """Enforce defining rules for the fold attribute
 
@@ -323,6 +562,7 @@ class FoldTrace(Trace):
         Rule 1) fold shape must match data shape
         Rule 2) fold data-type must match data data-type
         Rule 3) masked data are automatically 0 fold, but not vice versa
+        Rule 4) If fold has masked values, fill them with 0's
         """
         if not isinstance(self.fold, np.ndarray):
             raise TypeError('Fold must be a numpy.ndarray')   
@@ -331,9 +571,13 @@ class FoldTrace(Trace):
         # Enforce fold.dtype == data.dtype
         if self.data.dtype != self.fold.dtype:
             self.fold.dtype = self.fold.dtype.astype(self.data.dtype)
-        # Enforce masked values = 0 fold
+        # Enforce masked data values = 0 fold
         if isinstance(self.data, np.ma.MaskedArray):
             self.fold[self.data.mask] = 0
+        # Enforce masked fold values -> 0 fold
+        if isinstance(self.fold, np.ma.MaskedArray):
+            self.fold.fill_value = 0
+            self.fold = self.fold.filled()
 
     def _get_fold_view_trace(self):
         """Create a new FoldTrace object with data that houses
@@ -347,6 +591,10 @@ class FoldTrace(Trace):
         ftr = FoldTrace(data=self.fold, header=self.stats.copy())
         return ftr
 
-        
-
                          
+    def get_ext_id(self):
+        rstr = self.get_id()
+        rstr += f'.{self.stats.model}.{self.stats.weight}'
+        return rstr
+    
+    id = property(get_ext_id)

27df2403654a483adec8b77643d3c827fc602ad8
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2024-09-30 20:25:48 -0700

    Initial commit

---
+++ PULSE/test/data/util.py
@@ -0,0 +1,23 @@
+import os
+from obspy import read, Trace
+import numpy as np
+
+def load_townsend_example():
+    data = os.path.join('..','files','uw61965081_PortTownsend_M4.3.mseed')
+    st = read(data)
+    return st
+
+def load_logo_trace():
+    data = [0,0,0,2,4,6,4,2,0,-2,-4,-6,-4,-2,0,-1,-2,-1,0,-1,-2,-1,0,0,0,0,0]
+    tr = Trace(data=data,header={'network':'UW','station':'PULSE','location':'--','channel':'LGO'})
+    return tr
+
+def make_gappy_trace(tr,fill_value=-999, frac1=0.25, frac2=0.5):
+    ii = int(round(frac1*tr.stats.npts))
+    jj = int(round(frac2*tr.stats.npts))
+    mtr = tr.copy()
+    mtr.data = np.ma.MaskedArray(data=mtr.data,
+                                 mask=[False]*tr.stats.npts,
+                                 fill_value=fill_value)
+    mtr.data.mask[ii:jj] = True
+    return mtr

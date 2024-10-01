b126f096e1b223ea60c75f4a09b95d9fc6d69916
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2024-09-30 21:44:39 -0700

    Initial Commit

--- PULSE/test/data/util.py
+++ PULSE/test/data/util.py
@@ -7,9 +7,16 @@ def load_townsend_example():
     st = read(data)
     return st
 
+def load_logo_vector():
+    data = [0,0,0,2,4,6,4,2,0,-2,-4,-6,-4,-2,0,-1,-2,-1,0,-1,-2,-1,0,0,1,0,0,-1,0,0,0]
+    return np.array(data)
+
 def load_logo_trace():
-    data = [0,0,0,2,4,6,4,2,0,-2,-4,-6,-4,-2,0,-1,-2,-1,0,-1,-2,-1,0,0,0,0,0]
-    tr = Trace(data=data,header={'network':'UW','station':'PULSE','location':'--','channel':'LGO'})
+    tr = Trace(data=load_logo_vector(),
+               header={'network':'UW',
+                       'station':'PULSE',
+                       'location':'--',
+                       'channel':'LGO'})
     return tr
 
 def make_gappy_trace(tr,fill_value=-999, frac1=0.25, frac2=0.5):

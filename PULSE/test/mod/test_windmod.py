f3fc88c9c9c1aca4a9e9d3e98d907af351fb4346
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2024-12-10 14:10:49 -0800

    Add setup teardown

--- PULSE/test/mod/test_windmod.py
+++ PULSE/test/mod/test_windmod.py
@@ -6,7 +6,14 @@ from PULSE.data.dictstream import DictStream
 from PULSE.test.example_data import load_seattle_example
 
 class TestWindMod(unittest.TestCase):
+    st, _, _ = load_seattle_example(Path().cwd())
     def setUp(self):
-        st, _, _ = load_seattle_example(Path().cwd())
-        self.st = st
+        self.ds = DictStream(self.st.copy())
         self.test_mod = WindMod()
+    
+    def tearDown(self):
+        del self.ds
+        del self.test_mod
+
+    def test___init__(self):
+        self.assert
